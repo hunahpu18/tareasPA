bbfa98dc55410b535856e62e70ee03f97aeb9e25
hunahpu18 <101674270+hunahpu18@users.noreply.github.com>
2022-04-28 02:35:48 -0500

    se finaliza el programa de grupos falta agregar docstrings para los metodos de la clase

--- tarea7/main.py
+++ tarea7/main.py
@@ -1,108 +1,19 @@
-# This is a sample Python script.
+from group import *
+test = {('e', 'e'): 'e', ('e', 'a'): 'a', ('a', 'e'): 'a', ('a', 'a'): 'e'}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-class Group:
-    def __init__(self, table: dict):
-        self.elements = list(set(table.values()))
-        print(f'{table =}')
-        if self._has_unity(table) and self._has_inverses(table) and self._is_asociative(table):
-            self.table = table
-        else:
-            raise Exception("La tabla de multiplicación no es de un grupo")
+dieh = {('e', 'e'): 'e', ('e', 'a'): 'a', ('e', 'b'): 'b', ('e', 'c'): 'c', ('e', 'd'): 'd', ('e', 'f'): 'f',
+        ('a', 'e'): 'a', ('a', 'a'): 'b', ('a', 'b'): 'e', ('a', 'c'): 'd', ('a', 'd'): 'f', ('a', 'f'): 'c',
+        ('b', 'e'): 'b', ('b', 'a'): 'e', ('b', 'b'): 'a', ('b', 'c'): 'f', ('b', 'd'): 'c', ('b', 'f'): 'd',
+        ('c', 'e'): 'c', ('c', 'a'): 'f', ('c', 'b'): 'd', ('c', 'c'): 'e', ('c', 'd'): 'b', ('c', 'f'): 'a',
+        ('d', 'e'): 'd', ('d', 'a'): 'c', ('d', 'b'): 'f', ('d', 'c'): 'a', ('d', 'd'): 'e', ('d', 'f'): 'b',
+        ('f', 'e'): 'f', ('f', 'a'): 'd', ('f', 'b'): 'c', ('f', 'c'): 'b', ('f', 'd'): 'a', ('f', 'f'): 'e'}
 
-    def _is_asociative(self, table: dict) -> bool:
-        for i in self.elements:
-            for j in self.elements:
-                for k in self.elements:
-                    if table[(table[(i, j)], k)] != table[(i, table[(j, k)])]:
-                        print('asoc: no')
-                        return False
-        print('asoc: yes')
-        return True
-
-    def _has_unity(self, table: dict) -> bool:
-        n_of_units = 0
-        for i in self.elements:
-            is_unity = True
-            for j in self.elements:
-                if not (table[(i, j)] == j and table[(j, i)] == j):
-                    is_unity = False
-                    break
-            if is_unity:
-                n_of_units += 1
-                print(f'{i} es unidad')
-        if n_of_units == 1:
-            print('unidad: yes')
-            return True
-        else:
-            print('unidad: no')
-            return False
-
-    def _has_inverses(self, table: dict) -> bool:
-        for elto in self.elements:
-            row = [r for r in table.keys() if r[0] == elto]
-            row_values = [table[r] for r in row]
-            if ('e' in row_values) or (0 in row_values):
-                print(f'{elto} tiene inverso')
-            else:
-                print(f'{elto} inv: no')
-                return False
-        print('inv: yes')
-        return True
-
-    def show(self):
-        res = list()
-        for _ in self.elements:
-            aux = list()
-            for y in self.elements:
-                aux.append(y)
-            res.append(aux)
-        print(res)
-
-    def is_commutative(self) -> bool:
-        for i,j in self.table.keys():
-            if self.table[(i, j)]!=self.table[(j, i)]:
-                return False
-        return True
-    def order(self, elto) -> int:
-        if not elto in self.elements:
-            return -1
-        else:
-            aux=elto
-            for i in range(1,1+len(self.elements)):
-                aux = self.table[(elto,aux)]
-                if (aux == 0 or aux == 'e'):
-                    return i+1
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-def multtable(n):
-    elements = range(0,n)
-    res = dict()
-    for x in elements:
-        for y in elements:
-            res[(x,y)]=(x+y)%n;
-    return res;
-test = {(0, 0): 0, (0, 1): 1, (1, 0): 1, (1, 1): 0}
-test1 = {('e', 'e'): 'e', ('e', 'a'): 'a', ('a', 'e'): 'a', ('a', 'a'): 'e'}
-testd = {(0, 0): 0, (0, 1): 1, (0, 2): 2, (0, 3): 3, (0, 4): 4, (0, 5): 5,
-         (1, 0): 1, (1, 1): 2, (1, 2): 3, (1, 3): 4, (1, 4): 5, (1, 5): 0,
-         (2, 0): 2, (2, 1): 3, (2, 2): 4, (2, 3): 5, (2, 4): 0, (2, 5): 1,
-         (3, 0): 3, (3, 1): 4, (3, 2): 5, (3, 3): 0, (3, 4): 1, (3, 5): 2,
-         (4, 0): 4, (4, 1): 5, (4, 2): 0, (4, 3): 1, (4, 4): 2, (4, 5): 3,
-         (5, 0): 5, (5, 1): 0, (5, 2): 1, (5, 3): 2, (5, 4): 3, (5, 5): 4}
-
-print(multtable(2))
-## validate_multiplication_table(test)
-G = Group(multtable(5))
-G.show()
-print(f'G es conmutativo: {G.is_commutative()}')
-print(G.order(2))
-# Press the green button in the gutter to run the script.
-## if __name__ == '__main__':
-##    print_hi('PyCharm')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+test = mult_table(6)
+g = Group(dieh)
+g1 = Group(dieh)
+print(f'El grupo tiene orden: {g.order()}')
+print(f'El grupo es conmutativo {g.is_commutative()}')
+print(f'La operación de c - a es {g["c"] + g["a"]}')
+print(f'La operación de c - a es {g["c"] + g1["a"]}')
+print(f'{g}')
